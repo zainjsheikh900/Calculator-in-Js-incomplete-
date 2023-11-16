58dc2f82bc6220f7d25f3c02aa949f35f626d83a
Sheikh Zain Jaffer <136816448+zainjsheikh900@users.noreply.github.com>
2023-11-16 02:25:00 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+function dabaya(e){
+    document.getElementById('inp').value += e
+}
+
+function eq(){
+    var a = document.getElementById('inp')
+    a.value = eval (a.value)
+
+}
+
+function clr(){
+    document.getElementById('inp').value = ""
+
+}
+
+
+
+
+// function abc(){
+    
+//     document.getElementById('ttt').innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro amet doloribus molestias ex alias obcaecati
+//     labore eveniet itaque'
+//     document.getElementById('anc').style.display = "none"
+
+
+// }
